5e538d35455a226e6898a429ce6e82c82ec13df9
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-05-28 20:46:42 +0430

    imported our models in http service

--- video-game-db/src/app/services/http.service.ts
+++ video-game-db/src/app/services/http.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { environment as env} from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { environment as env } from '../../environments/environment';
+import { Game, APIResponse } from '../models';
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
   constructor(private http: HttpClient) {}
 
-  getGameList(ordering: string, search?: string)Observable<APIResponse<Game>> {
-
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
     const params = new HttpParams().set('ordering', ordering);
 
-    if(search) {
-      const params = new HttpParams().set('ordering', ordering).set('search', search);
+    if (search) {
+      const params = new HttpParams()
+        .set('ordering', ordering)
+        .set('search', search);
     }
 
     return this.http.get<APIResponse<Game>>(`${env.BASE_URL}`, {
